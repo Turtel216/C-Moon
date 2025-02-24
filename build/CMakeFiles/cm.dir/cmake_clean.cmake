a34b25a8ebacc278e827932a04c9148f3f77eab9
Turtel216 <papadimitrios216@gmail.com>
2025-02-24 19:34:56 +0200

    fix build files

---
+++ build/CMakeFiles/cm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cm.dir/link.d"
+  "CMakeFiles/cm.dir/src/main.cpp.o"
+  "CMakeFiles/cm.dir/src/main.cpp.o.d"
+  "cm"
+  "cm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dimitrios/Documents/repos/cmoon
+# Build directory: /home/dimitrios/Documents/repos/cmoon/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
