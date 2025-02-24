2f0da88d0a480257173c52b6a77a79d7c70c6cce
Turtel216 <papadimitrios216@gmail.com>
2025-02-24 19:36:10 +0200

    remove build files from git

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dimitrios/Documents/repos/cmoon
-# Build directory: /home/dimitrios/Documents/repos/cmoon/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
